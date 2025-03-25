3c4ee6dbac4cd115f2531e09245b8f46eeb3cbb6
EvgeniiBuller <buller.1@gmx.de>
2025-03-25 19:23:56 +0100

    add homework 06

---
+++ src/homeworks/Homework06/Homework06.tsx
@@ -0,0 +1,11 @@
+import Input from "../../components/Input/Input";
+
+function Homework06() {
+    return (
+        <div>
+          <Input placeholder="Введите данные" name="homeworkInput" />
+        </div>
+      );
+}
+
+export default Homework06;
