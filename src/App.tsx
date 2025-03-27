56d77d7cfe0ff6519d80d464351016884893c289
EvgeniiBuller <buller.1@gmx.de>
2025-03-27 17:23:13 +0100

    add consultation

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@
 //import Task6 from './lessons/Lesson06/Lesson06';
 //import Lesson06 from './lessons/Lesson06/Lesson06';
 //import Lesson07 from './lessons/Lesson06/Lesson07/Lesson07'
-
-import Lesson08 from "./lessons/Lesson08/Lesson08"
+// import Lesson08 from "./lessons/Lesson08/Lesson08"
 
 ////Homeworks imports
 //import Homework06 from './homeworks/Homework06/Homework06'
 // import Homework07 from "./homeworks/Homework07/Homework07"
+
+//consultation import
+import Consultation04 from "./consultations/Consultation04/Consultation04"
+
 function App() {
   
   return (
@@ -18,7 +21,8 @@ function App() {
     {/* <Homework06/> */}
         {/* <Lesson07/> */}
         {/* <Homework07/> */}
-       <Lesson08/>
+       {/* <Lesson08/> */}
+       <Consultation04/>
     </>
   )
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,5 @@
 
-import "./style.css";
+import "./styles.css";
 import { InputProps } from './types';
 
 function Input({ 
