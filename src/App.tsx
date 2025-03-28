2f6a48aa2e57344d8bb03232d7a9bdf2390b7aac
EvgeniiBuller <buller.1@gmx.de>
2025-03-28 14:32:57 +0100

    add Lesson09

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@
 //import Lesson06 from './lessons/Lesson06/Lesson06';
 //import Lesson07 from './lessons/Lesson06/Lesson07/Lesson07'
 // import Lesson08 from "./lessons/Lesson08/Lesson08"
+import Lesson09 from "./lessons/Lesson09/Lesson09"
 
 ////Homeworks import
 //import Homework06 from './homeworks/Homework06/Homework06'
 // import Homework07 from "./homeworks/Homework07/Homework07"
-import Homework08 from "./homeworks/Homework08/Homework08"
+//import Homework08 from "./homeworks/Homework08/Homework08"
 
 //consultation import
 //import Consultation04 from "./consultations/Consultation04/Consultation04"
@@ -24,7 +25,8 @@ function App() {
         {/* <Homework07/> */}
        {/* <Lesson08/> */}
        {/* <Consultation04/> */}
-       <Homework08/>
+       {/* <Homework08/> */}
+       <Lesson09 />
     </>
   )
 }
